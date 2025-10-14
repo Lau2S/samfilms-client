6f4c9a285dd23f2b43d45908f7d6bf18aa21b34b
Laura Salazar <laura.blanco@correounivalle.edu.co>
2025-10-14 14:30:10 -0500

    AbotPage implemented

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AboutPage.scss";
+import { useNavigate } from "react-router";
 
 /**
  * About page describing the CrunchyEISC team and purpose.
@@ -8,10 +9,27 @@ import "./AboutPage.scss";
  * @returns {JSX.Element} A brief description about the team.
  */
 const AboutPage: React.FC = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Sobre nosotros</h1>
-      <h2>Conoce a nuestro equipo </h2>
+     <div className="home-page">
+      {/* Hero Section con fondo borroso de películas */}
+      <section className="hero-section">
+        <div className="hero-overlay"></div>
+        <div className="hero-content">
+          <h1 className="hero-title">
+            Sobre Nosotros
+          </h1>
+          <p className="hero-subtitle">
+            Somos un pequeño equipo de desarrollo amantes de las 
+            <br />
+            peliculas y el desarrollo web, hemos creado esta aplicación <br/>
+            para que otras personas que al igual que nosotros aman el <br/>
+            cine puedan disfrutar de sus peliculas favoritas de forma <br/>
+            gratuita y sin interrupciones.
+          </p>
+          <button className="cta-button" onClick={() => navigate("/inicio-sesion")}>Comenzar</button>
+        </div>
+      </section>
     </div>
   );
 };
